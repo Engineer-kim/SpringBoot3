c0b78547c877c06eef6d6dd740ceb9fdc22cc8b9
Engineer-kim <ddang0103@naver.com>
2024-11-27 17:44:34 +0900

    Feature: JPA 가 아닌 EntityManger 를 통한 CRUD
    까먹었지만 새로 배운점:
    @Transactional 어노테이션이 붙으면
    해당 메서드는 Persist 만 해도 flush가 자동으로 호출되어 DB관련 연산 수행 가능

--- JPA_CRUD/umm/src/main/java/com/toyproject/umm/UmmApplication.java
+++ JPA_CRUD/umm/src/main/java/com/toyproject/umm/UmmApplication.java
@@ -1,10 +1,14 @@
 package com.toyproject.umm;
 
+import com.toyproject.umm.dao.StudentDao;
+import com.toyproject.umm.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class UmmApplication {
@@ -14,10 +18,91 @@ public class UmmApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-			System.out.println("Welcome to ToyProject");
+	public CommandLineRunner commandLineRunner(StudentDao studentDao) {
+
+		return runner -> {
+			createStudent(studentDao);
+
+			createMultipleStudents(studentDao);
+
+			readStudent(studentDao);
+
+			 queryForStudents(studentDao);
+
+			 queryForStudentsByLastName(studentDao);
+
+			 updateStudent(studentDao);
+
+			 deleteStudent(studentDao);
+
+			 deleteAllStudents(studentDao);
 		};
 	}
 
+	private void deleteAllStudents(StudentDao studentDao) {
+		System.out.println("Deleting all students");
+		int numRowsDeleted = studentDao.deleteAll();
+		System.out.println("Deleted row count: " + numRowsDeleted);
+	}
+
+	private void deleteStudent(StudentDao studentDao) {
+		int studentId = 3;
+		System.out.println("Deleting student id: " + studentId);
+		studentDao.delete(studentId);
+	}
+
+	private void updateStudent(StudentDao studentDao) {
+		int studentId = 1;
+		System.out.println("Getting student with id: " + studentId);
+		Student myStudent = studentDao.findById(studentId);
+		System.out.println("Updating student ...");
+		myStudent.setFirstName("John");
+		studentDao.update(myStudent);
+		System.out.println("Updated student: " + myStudent);
+	}
+
+	private void queryForStudentsByLastName(StudentDao studentDao) {
+		List<Student> theStudents = studentDao.findByLastName("Doe");
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void queryForStudents(StudentDao studentDao) {
+		List<Student> theStudents = studentDao.findAll();
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void readStudent(StudentDao studentDao) {
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Daffy", "Duck", "daffy@luv2code.com");
+		System.out.println("Saving the student ...");
+		studentDao.save(tempStudent);
+		int theId = tempStudent.getId();
+		System.out.println("Saved student. Generated id: " + theId);
+		System.out.println("Retrieving student with id: " + theId);
+		Student myStudent = studentDao.findById(theId);
+		System.out.println("Found the student: " + myStudent);
+	}
+
+	private void createMultipleStudents(StudentDao studentDao) {
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
+		System.out.println("Saving the students ...");
+		studentDao.save(tempStudent1);
+		studentDao.save(tempStudent2);
+		studentDao.save(tempStudent3);
+	}
+
+	private void createStudent(StudentDao studentDao) {
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+		System.out.println("Saving the student ...");
+		studentDao.save(tempStudent);
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+	}
 }
---
+++ JPA_CRUD/umm/src/main/java/com/toyproject/umm/dao/StudentDaoImpl.java
@@ -0,0 +1,65 @@
+package com.toyproject.umm.dao;
+
+import com.toyproject.umm.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class StudentDaoImpl implements StudentDao {
+    private final EntityManager entityManager;
+
+    @Autowired
+    public StudentDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    @Override
+    @Transactional
+    //해당 메소드가 종료될 때 트랜잭션이 커밋되면서 자동으로 flush가 발생합니다.
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        TypedQuery<Student> theQuery = entityManager.createQuery(
+                "FROM Student WHERE lastName=:theData", Student.class);
+        theQuery.setParameter("theData", theLastName);
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        Student theStudent = entityManager.find(Student.class, id);
+        entityManager.remove(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        int numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+        return numRowsDeleted;
+    }
+}
